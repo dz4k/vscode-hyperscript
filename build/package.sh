7b4a7f148409fbe73698819a880d75b93d3c5cff
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-02-18 15:43:57 +0300

    add packaging script

---
+++ build/package.sh
@@ -0,0 +1,26 @@
+
+set -e
+
+version=$(jq <package.json '.version')
+
+build_vscode() {
+	if ! which vsce >/dev/null
+	then
+		echo "Please install vsce to build VSCode package: https://github.com/microsoft/vscode-vsce"
+	fi
+
+	vsce package -o dist/vscode-hyperscript-org-${version}.vsix
+}
+
+build_subl() {
+	cd syntaxes
+	zip ../dist/_hyperscript.sublime-package \
+		'_hyperscript.tmLanguage' \
+		'HTML (_hyperscript).sublime-syntax'
+	cd ..
+}
+
+[ -d dist ] || mkdir dist
+npm run build
+build_vscode
+build_subl
